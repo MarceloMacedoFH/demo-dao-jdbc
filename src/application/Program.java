19e7ea53a8add7fb21b9d940925c12967a164249
Marcelo Macedo Filho <marcelomf2702@hotmail.com>
2025-08-31 00:11:20 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,10 +14,8 @@ public class Program {
 		Department obj = new Department(1,"Books");
 		
 		Seller seller = new Seller(1, "Marcelo", "teste", new Date( ), 2000.0, obj);
-		
-		System.out.println(seller);
-		
-		
+				
+		SellerDao sellerDao = DaoFactory.createSellerDao(); 
 		
 		
 	}
