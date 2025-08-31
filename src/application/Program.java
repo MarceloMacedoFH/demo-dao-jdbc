a239c5857c1066396db1df4d614ac2de5bc269a9
Marcelo Macedo Filho <marcelomf2702@hotmail.com>
2025-08-30 23:57:54 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,23 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1,"Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "Marcelo", "teste", new Date( ), 2000.0, obj);
+		
+		System.out.println(seller);
+		
+		
+		
+		
 	}
 
 }
